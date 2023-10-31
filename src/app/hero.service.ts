f39ba84ba84ebfe9dc9d94e99b35620758893e82
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-10-31 16:01:04 +0100

    display the message from HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,6 +11,7 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 
